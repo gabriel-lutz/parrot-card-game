e671c31c099f2e73e9a626313c01959f984f27cd
Gabriel Peres Lutz <gabriel.p.lutz@gmail.com>
2021-04-06 19:42:40 -0300

    Permite a selecao de 2 cards

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,37 @@
-
+let jogadaRound = 0;
 let cartas = Number(prompt("Com quantas cartas será o seu jogo?"));
 while((cartas%2) !== 0 || cartas < 4 || cartas > 14){
     cartas = Number(prompt("Com quantas cartas será o seu jogo?"));
 }
 
-function colocarCartas(){
+function colocarCartas(numeroCarta){
     const colocar = document.querySelector(".tabuleiro");
     if(colocar.innerHTML !== null){
-        colocar.innerHTML = colocar.innerHTML + "<div class='card'><img src='imagens/front.png' alt=''></div>"
+        colocar.innerHTML = colocar.innerHTML + `<div class='carta' onclick='virar(${numeroCarta})'><img src='imagens/front.png' alt=''></div>`
     }else{
-        colocar.innerHTML = "<div class='card'><img src='imagens/front.png' alt=''></div>"
+        colocar.innerHTML = `<div class='carta' onclick='virar(${numeroCarta})'><img src='imagens/front.png' alt=''></div>`
     }
 }
 
 for(let i =0 ; i < cartas; i++){
-colocarCartas();
+colocarCartas(i);
 }
+
+function virar(numeroCarta){
+    if(jogadaRound === 0){
+        const virado = document.querySelector(`.carta:nth-child(${numeroCarta+1})`)
+        virado.classList.add("selecionado");
+        jogadaRound++;
+    }else if(jogadaRound === 1){
+        const virado = document.querySelector(`.carta:nth-child(${numeroCarta+1})`)
+        virado.classList.add("selecionado");
+        jogadaRound++;
+    }else{
+        for(let i=0; i <2; i++){
+        const reset = document.querySelector(".selecionado")
+        reset.classList.remove("selecionado");
+        }
+        jogadaRound = 0;
+    }
+}
+
