9cfd19cef83a0ebf31b64618b29c803aab309763
Gabriel Peres Lutz <gabriel.p.lutz@gmail.com>
2021-04-07 19:05:12 -0300

    Randomizador de cartas

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,55 @@
 let jogadaRound = 0;
+let tiposCartasTotal = ["imagens/bobrossparrot.gif","imagens/explodyparrot.gif", 
+                        "imagens/fiestaparrot.gif", "imagens/metalparrot.gif",
+                        "imagens/revertitparrot.gif", "imagens/tripletsparrot.gif",
+                        "imagens/unicornparrot.gif" ];
+const parCartas = ["A", "B", "C", "D", "E", "F", "G"];
+let divEmbaralhada = [];
+
 let cartas = Number(prompt("Com quantas cartas será o seu jogo?"));
 while((cartas%2) !== 0 || cartas < 4 || cartas > 14){
     cartas = Number(prompt("Com quantas cartas será o seu jogo?"));
 }
 
-function colocarCartas(numeroCarta){
-    const colocar = document.querySelector(".tabuleiro");
-    if(colocar.innerHTML !== null){
-        colocar.innerHTML = colocar.innerHTML + `<div class='carta' onclick='virar(${numeroCarta})'><img src='imagens/front.png' alt=''></div>`
-    }else{
-        colocar.innerHTML = `<div class='carta' onclick='virar(${numeroCarta})'><img src='imagens/front.png' alt=''></div>`
-    }
+for(let i =0 ; i < cartas/2; i++){
+    colocarCartasDiv(tiposCartasTotal[i], parCartas[i]);
+}
+divEmbaralhada.sort(comparador)
+alert(divEmbaralhada)
+colocarCartas();
+
+function colocarCartasDiv(endereco, par){
+    divEmbaralhada.push(`<div class='carta ${par}' onclick='virar(this)'><img src='${endereco}' alt=''></div>`)
+    divEmbaralhada.push(`<div class='carta ${par}' onclick='virar(this)'><img src='${endereco}' alt=''></div>`)
 }
 
-for(let i =0 ; i < cartas; i++){
-colocarCartas(i);
+function colocarCartas(){
+    const colocar = document.querySelector(".tabuleiro");
+    colocar.innerHTML =  divEmbaralhada;
 }
 
 function virar(numeroCarta){
-    if(jogadaRound === 0){
-        const virado = document.querySelector(`.carta:nth-child(${numeroCarta+1})`)
-        virado.classList.add("selecionado");
+    if(jogadaRound === 0){     
+        numeroCarta.classList.add("selecionado");
         jogadaRound++;
-    }else if(jogadaRound === 1){
-        const virado = document.querySelector(`.carta:nth-child(${numeroCarta+1})`)
-        virado.classList.add("selecionado");
+    }else{ 
+        numeroCarta.classList.add("selecionado");
         jogadaRound++;
-    }else{
-        for(let i=0; i <2; i++){
-        const reset = document.querySelector(".selecionado")
-        reset.classList.remove("selecionado");
-        }
-        jogadaRound = 0;
+    }
+        if(jogadaRound === 2){               
+            setTimeout(function(){
+                for(let i=0; i <2; i++){
+                const reset = document.querySelector(".selecionado")
+                reset.classList.remove("selecionado");
+                }
+                jogadaRound = 0;
+        }, 1000)
     }
 }
+
+
+function comparador(){
+    return Math.random() - 0.5;
+}
+
 
